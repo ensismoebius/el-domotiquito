f85816a250c2fa0254118747dc1603945142954c
Andre Furlan <ensismoebius@gmail.com>
2023-01-09 05:50:52 -0300

    Falha ao compilar quando da criação dos links simbolicos

--- domoInterface/config.h
+++ domoInterface/config.h
@@ -1,1 +1,8 @@
-config.h
+#define TAXA_DE_TRANSFERENCIA_SERIAL 9600
+#define NIVEL_LIMITE_DE_UMIDADE 100
+#define PINO_DO_SENSOR_DE_CHUVA A0
+#define FINALIZADOR_DE_STRING '\0'
+#define RX 10
+#define TX 11
+const char *SSID = "LulaPresidente_ForaCentrao";
+const char *PASSWD = "@@$UP&R4";
